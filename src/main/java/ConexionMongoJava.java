c5ce7b62e49c770fa761db81c9234e1e3954e017
alumno <dam.sonia.fernandez@gmail.com>
2023-02-19 13:48:42 +0100

    19.02.23

--- src/main/java/ConexionMongoJava.java
+++ src/main/java/ConexionMongoJava.java
@@ -2,23 +2,24 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
 import static com.mongodb.client.model.Accumulators.avg;
 import static com.mongodb.client.model.Accumulators.sum;
-import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Sorts.*;
 import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Aggregates.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.logging.Filter;
 
-import static com.mongodb.client.model.Filters.eq;
+import java.io.*;
+import java.util.*;
+import java.util.logging.Filter;
 
 public class ConexionMongoJava {
     public static void main(String[] args) {
@@ -161,7 +162,7 @@ public class ConexionMongoJava {
 
         //UTILIZAR PROYECCIONES:
 
-        //es importante importar lo métodos de las clases Sort: import static com.mongodb.client.model.Projections.*;
+        //es importante importar lo métodos de las clases Protections: import static com.mongodb.client.model.Projections.*;
         System.out.println("\n*** UTILIZAR PROYECCIONES ***");
         MongoCursor<Document> docs2 = colection.find(eq("curso","1DAM")).sort(ascending("nombre")).projection(include("nombre","nota")).iterator();
         while (docs2.hasNext()){
@@ -173,7 +174,7 @@ public class ConexionMongoJava {
 
         //UTILIZAR AGREGACIONES:
 
-        //es importante importar lo métodos de las clases Sort: import static com.mongodb.client.model.Aggregates.*;
+        //es importante importar lo métodos de las clases Aggregates: import static com.mongodb.client.model.Aggregates.*;
         System.out.println("\n*** UTILIZAR AGREGACIONES ***");
 
         //FORMA 1
@@ -202,5 +203,153 @@ public class ConexionMongoJava {
             System.out.println(docu.toJson());
         }
         docs5.close();
+
+        //FORMA 4
+        MongoCursor<Document> docs6 =  colection.aggregate(Arrays.asList(group("$curso",sum("sumanota","$nota"),avg("medianota","$nota")),out("medianota"))).iterator();
+        while (docs6.hasNext()){
+            Document docu = docs6.next();
+            System.out.println("- Forma 4: usando la visualización anterior, almacenando los datos recogidos en una coleción nueva usando 'out'");
+            System.out.println(docu.toJson());
+        }
+        docs6.close();
+
+        //ACTUALIZAR DOCUMENTOS:
+
+        //es importante importar los métodos de las clases Updates: import static com.mongodb.client.model.Updates.*;
+        System.out.println("\n*** ACTUALIZAR DOCUMENTOS ***");
+
+        //FORMA 1
+        System.out.println("- Forma 1: actualizamos a Ana su nota con un 5, usando  updateOne ");
+        colection.updateOne(eq("nombre","Ana"),set("nota",5));
+
+        //FORMA 2
+        System.out.println("- Forma 2: actualizamos el curso 1DAM con una nota de 3 usando updateMany");
+        UpdateResult updateResult = colection.updateMany(eq("curso","1DAM"),inc("nota",3));
+        System.out.println("\n - Se han modificado: " +  updateResult.getModifiedCount());
+        System.out.println(" - Se han seleccionado: " +  updateResult.getMatchedCount());
+        System.out.println("\n  -- Actualizando todos los datos de una colección con la nota de  2 , usando la función exists(''_id'') --");
+        UpdateResult updateResult2 = colection.updateMany(exists("_id"),inc("nota",2));
+        System.out.println("\n - Se han modificado: " +  updateResult2.getModifiedCount());
+        System.out.println("\n  -- Añadir un campo, usando la función 'se', no existe, no se crea. Añadimos a Marleni : 'poblacion = Toledo' --");
+        UpdateResult updateResult3 = colection.updateOne(eq("nombre","Marleni"),set("poblacion","Toledo"));
+        System.out.println("\n - Se han modificado: " +  updateResult3.getModifiedCount());
+        System.out.println("\n  -- Eliminar un campo utilizando 'unset'. Borramos los campos de '1DAM' que tengan 'poblacion' --");
+        UpdateResult updateResult4 = colection.updateOne(eq("nombre","1DAM"),unset("poblacion"));
+        System.out.println("\n - Se han modificado: " +  updateResult4.getModifiedCount());
+
+        //BORRAR UN DOCUMENTO DE LA COLECCION:
+        System.out.println("\n*** BORRAR UN DOCUMENTO DE LA COLECCION ***");
+
+        //Borramos un documento
+        System.out.println(" -Borramos un documento que contenga 'nombre = Ana' ");
+        DeleteResult del = colection.deleteOne(eq("nombre","Ana"));
+        System.out.println("   Se ha borrado: "+del.getDeletedCount());
+
+        //Borrar todos
+        System.out.println(" -Borramos todos");
+        del = colection.deleteMany(exists("_id"));
+        System.out.println("   Se han borrado: "+del.getDeletedCount());
+
+        //CREAR Y BORRAR UN COLECCION:
+        System.out.println("\n*** CREAR Y BORRAR UN COLECCION ***");
+
+        System.out.println(" -Creamos una nueva coleccion 'nuevaColeccion2' en 'mibasededatos', creando un nuevo cliente 'cliente2' primero");
+        MongoClient client2 = new MongoClient();
+        MongoDatabase db2 = client2.getDatabase("mibasededatos");
+        db2.createCollection("nuevaColeccion2");
+        MongoCollection<Document> cnueva = db2.getCollection("nuevaColeccion2");
+
+        //La borramos
+        System.out.println(" -Borraos la colección creada");
+        cnueva.drop();
+
+        //LISTAR LAS COLECCIONES DE LA BD
+        System.out.println("\n*** LISTAR LAS COLECCIONES DE LA BD ***");
+
+        System.out.println("  -Listado de colecciones:");
+        MongoIterable<String> coleciones = db.listCollectionNames();
+        Iterator col= coleciones.iterator();
+        while (col.hasNext()){
+            System.out.println("    FORMA 1:");
+            System.out.println(col.next());
+        }
+
+        //Tambien se puede hacer de esta forma
+        for (String name : db.listCollectionNames()){
+            System.out.println("    FORMA 2:");
+            System.out.println(name);
+        }
+
+        //CREAR, LISTAR Y BORRAR BD
+        System.out.println("\n*** CREAR, LISTAR Y BORRAR BD ***");
+
+        System.out.println(" -Creamos una BD = 'nueva'");
+        MongoClient client3 = new MongoClient();
+        MongoDatabase db3 = client3.getDatabase("nueva");
+        MongoCollection<Document> clnueva = db3.getCollection("coleccionNueva");
+        Document document = new Document("nombre","Pedro").append("telefono",1234).append("curso","2DAM");
+
+        System.out.println(" -Listamos los nombres de la BD");
+        for (String name: client3.listDatabaseNames()){
+            System.out.println(name);
+        }
+
+        System.out.println(" -Borramos la BD 'nueva' ");
+        client3.getDatabase("nueva").drop();
+
+        //PASAR DATOS DE MONGO A UN FICHERO DE TEXTO
+        System.out.println("\n*** PASAR DATOS DE MONGO A UN FICHERO DE TEXTO ***");
+
+        System.out.println(" -Crear una fichero con los datos de una coleccion:");
+        crearFichroJson();
+
+        System.out.println(" -Leer date de un fichero en Json, almacenandolo en una coleccion:");
+        leerFicheroyGuardarDatos();
+    }
+
+    private static void leerFicheroyGuardarDatos () {
+        try {
+            FileReader fr = new FileReader("./amigos.json");
+            BufferedReader bf = new BufferedReader(fr);
+            MongoClient client = new MongoClient();
+            MongoDatabase db = client.getDatabase("mibasodedatos");
+            MongoCollection<Document> collection = db.getCollection("amigosfile");
+            String cadenaJson;
+
+            while ((cadenaJson = bf.readLine()) != null){
+                System.out.println(cadenaJson);
+                Document docu = new Document(org.bson.Document.parse(cadenaJson));
+                collection.insertOne(docu);
+            }
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void crearFichroJson () {
+        System.out.println("**Creamos objetos: cliente, db, coleccion, fichero (amigos.json) y fic **");
+        MongoClient client = new MongoClient();
+        MongoDatabase db = client.getDatabase("mibasededatos");
+        MongoCollection<Document> collection = db.getCollection("amigos");
+        File fiche = new File("./amigos.json");
+        FileWriter fic;
+
+        try {
+            fic = new FileWriter(fiche);
+            BufferedReader ficheroBF = new BufferedReader(fic);
+            System.out.println("\n- Recorremos la colecion");
+            List<Document> consulta = collection.find().into(new ArrayList<Document>());
+
+            for (int i=0; i<consulta.size();i++){
+                System.out.println(" Grados elemento "+i+", "+consulta.get(i).toString());
+                ficheroBF.write(consulta.get(i).toJson());
+                ficheroBF.newLine();
+            }
+            ficheroBF.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
